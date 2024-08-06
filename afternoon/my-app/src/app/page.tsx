fc78059ea1807d925113ceebcb96478913d97bc0
bojana-petroska <bojana.petroska@yahoo.com>
2024-08-06 18:07:10 +0200

    with Provider

--- afternoon/my-app/src/app/page.tsx
+++ afternoon/my-app/src/app/page.tsx
@@ -1,47 +1,40 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { TaskList } from './components/TaskList';
-import { Task } from './interfaces';
 import { useTasks, TaskProvider } from './context/TaskContext';
 
-export default function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+function HomeContent() {
+  const [inputValue, setInputValue] = useState('');
+  const { tasks, addTask } = useTasks();
 
-  useEffect(() => {
-    const fetchedData = () => {
-      setTimeout(() => {
-        const fetchedTasks: Task[] = [
-          {
-            id: 1,
-            title: 'walk dogs',
-            completed: true,
-          },
-          {
-            id: 2,
-            title: 'buy food',
-            completed: true,
-          },
-          {
-            id: 3,
-            title: 'learn',
-            completed: false,
-          },
-          {
-            id: 4,
-            title: 'clean',
-            completed: false,
-          },
-        ];
-        setTasks(fetchedTasks);
-      }, 1000);
-    };
-
-    fetchedData();
-  }, []);
+  const handleAddTask = () => {
+      if (inputValue) {
+          addTask(inputValue);
+          setInputValue('');
+      }
+  };
 
   return (
-    <main>
-      <TaskList tasks={tasks} />
-    </main>
+      <TaskProvider>
+          <main>
+              <TaskList tasks={tasks} />
+              <input
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  placeholder="add new task"
+              />
+              <button onClick={handleAddTask}>add task</button>
+          </main>
+      </TaskProvider>
+  );
+}
+
+
+export default function Home() {
+  return (
+    <TaskProvider>
+      <HomeContent />
+    </TaskProvider>
   );
 }
